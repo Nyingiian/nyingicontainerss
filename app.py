6071f5e3f6441d90a073b3072909b2d3e227d9d4
mugane-dj <muganedev25@gmail.com>
2022-09-04 12:00:49 +0300

    initial deploy

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ with st.container():
     <form action="https://formsubmit.co/nyingicontainers@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value = "false">
         <input type="text" name="name" placeholder="Your name" required>
-        <input type="email" name="email" placeholer="Your email" required>
+        <input type="text" name="email" placeholer="Your email" required>
         <textarea name="message" placeholder="Your message here" required></textarea>
         <button type="submit">Send</button>
     </form> 
